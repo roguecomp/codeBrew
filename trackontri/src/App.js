fae4a08a311948710f3f40d83fc57cb95e411c15
roguecomp <roguecomp001@gmail.com>
2021-04-16 23:23:52 +1000

    reverting to original

--- trackontri/src/App.js
+++ trackontri/src/App.js
@@ -1,36 +1,30 @@
-import React from 'react';
-import Plot from 'react-plotly.js';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-    render() {
-        return ( <
-            Plot data = {
-                [{
-                        x: [1, 2, 3],
-                        y: [2, 6, 3],
-                        type: 'scatter',
-                        mode: 'lines+markers',
-                        marker: {
-                            color: 'red'
-                        },
-                    },
-                    {
-                        type: 'bar',
-                        x: [1, 2, 3],
-                        y: [2, 5, 3]
-                    },
-                ]
-            }
-            layout = {
-                {
-                    width: 320,
-                    height: 240,
-                    title: 'A Fancy Plot'
-                }
-            }
-            />
-        );
-    }
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = {
+            logo
+        }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
