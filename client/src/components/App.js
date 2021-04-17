d9a73e77f564ea5b57b475473ecddaf6623fed01
Jordan Ung <jordanung@protonmail.com>
2021-04-18 03:29:53 +1000

    Dashboard frontend components done - need to connect to live data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import logo from '../logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './LandingPage'
 import MainPage from './MainPage'
+import DashboardPage from './DashboardPage';
 import '../css/App.css';
 import React from 'react';
 import NavBar from './NavBar'
@@ -12,18 +13,20 @@ function App() {
   const [currentPage, setCurrentPage] = React.useState('home')
   return (
     <div className="App">
-      <NavBar hideLinks = {(currentPage =="landing")? true:false} setCurrentPage = {setCurrentPage}/>
+      <NavBar hideLinks = {(currentPage === "landing")? true:false} setCurrentPage = {setCurrentPage}/>
 
-      <div className ='landingPage' style = {{display : (currentPage =="landing")? 'block':'none'}}>
+      <div className ='landingPage' style = {{display : (currentPage === "landing")? 'block':'none'}}>
       <LandingPage/> 
       </div> 
-      <div className ='mainPage' style = {{display : (currentPage =="home")? 'block':'none'}}>
+      <div className ='mainPage' style = {{display : (currentPage === "home")? 'block':'none'}}>
       <MainPage />
       </div>
-      <div className ='teamPage' style = {{display : (currentPage =="team")? 'block':'none'}}>
+      <div className ='teamPage' style = {{display : (currentPage === "team")? 'block':'none'}}>
       <teamPage />
       </div>
-      
+      <div className ='dashboardPage' style = {{display : (currentPage === "dashboard")? 'block':'none'}}>
+      <DashboardPage />
+      </div>
 
       
     </div>
