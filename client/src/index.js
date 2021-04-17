d051b85f5f7f9a88a9a918b6396852724fe225d2
roguecomp <roguecomp001@gmail.com>
2021-04-17 13:11:58 +1000

    removed duplicates

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD:trackontri/src/index.js
-import './index.css';
-import App from './App';
-=======
 import './css/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> f5aa27a39aff8c894e345f4611c384ad6c55fafe:client/src/index.js
 
 ReactDOM.render(
   <React.StrictMode>
